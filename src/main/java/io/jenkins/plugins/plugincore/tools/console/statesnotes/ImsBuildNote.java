41718cac36d684545c1ab760a60605ac7df70dae
scherenhaenden <scherenhaenden@gmail.com>
2018-11-15 18:05:46 +0100

    Interface for the different RegexPatterns

---
+++ src/main/java/io/jenkins/plugins/plugincore/tools/console/statesnotes/ImsBuildNote.java
@@ -0,0 +1,8 @@
+package io.jenkins.plugins.plugincore.tools.console.statesnotes;
+
+import java.util.regex.Pattern;
+
+// Filename: Ims.java
+public interface ImsBuildNote {
+    public Pattern getNotePattern();
+}
