e7879cd2459732fa3d8d156d101a13d07bcb2889
scherenhaenden <scherenhaenden@gmail.com>
2018-11-29 21:24:05 +0100

    Warning parser added

---
+++ src/main/java/io/jenkins/plugins/sample/MSBuildWarningNote.java
@@ -0,0 +1,36 @@
+package io.jenkins.plugins.sample;
+
+import hudson.Extension;
+import hudson.MarkupText;
+import hudson.console.ConsoleAnnotationDescriptor;
+import hudson.console.ConsoleAnnotator;
+import hudson.console.ConsoleNote;
+import org.jenkinsci.Symbol;
+
+import java.util.regex.Pattern;
+
+/**
+ * Annotation for MSBuild warning messages
+ */
+public class MSBuildWarningNote extends ConsoleNote {
+    /** Pattern to identify warning messages */
+    public final static Pattern PATTERN = Pattern.compile("(.*)\\(\\d+(,\\d+){0,1}\\):\\s[Ww]arning\\s(([A-Z]*)\\d+){0,1}:\\s(.*)");
+    
+    public MSBuildWarningNote() {
+    }
+
+    @Override
+    public ConsoleAnnotator annotate(Object context, MarkupText text, int charPos) {
+        text.addMarkup(0, text.length(), "<span class=warning-inline>", "</span>");
+        return null;
+    }
+
+    @Extension @Symbol("msbuildWarning")
+    public static final class DescriptorImpl extends ConsoleAnnotationDescriptor {
+
+        @Override
+        public String getDisplayName() {
+            return Messages.MsBuildBuilder_WarningNoteDescription();
+        }
+    }
+}
