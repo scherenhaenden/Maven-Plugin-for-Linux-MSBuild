be716cf4f13de32fe3608f7f66e6aa22cd71e289
scherenhaenden <scherenhaenden@gmail.com>
2018-11-29 21:24:20 +0100

    Error Parser added

---
+++ src/main/java/io/jenkins/plugins/sample/MSBuildErrorNote.java
@@ -0,0 +1,36 @@
+package io.jenkins.plugins.sample;
+
+import hudson.Extension;
+import hudson.MarkupText;
+import hudson.console.ConsoleAnnotationDescriptor;
+import hudson.console.ConsoleAnnotator;
+import hudson.console.ConsoleNote;
+import org.jenkinsci.Symbol;
+
+import java.util.regex.Pattern;
+
+/**
+ * Annotation for MSBuild and CSC error messages
+ */
+public class MSBuildErrorNote extends ConsoleNote {
+    /** Pattern to identify error messages */
+    public final static Pattern PATTERN = Pattern.compile("(.*)[Ee]rror\\s(([A-Z]*)\\d+){0,1}:\\s(.*)");
+    
+    public MSBuildErrorNote() {
+    }
+
+    @Override
+    public ConsoleAnnotator annotate(Object context, MarkupText text, int charPos) {
+        text.addMarkup(0, text.length(), "<span class=error-inline>", "</span>");
+        return null;
+    }
+
+    @Extension @Symbol("msbuildError")
+    public static final class DescriptorImpl extends ConsoleAnnotationDescriptor {
+
+        @Override
+        public String getDisplayName() {
+            return Messages.MsBuildBuilder_ErrorNoteDescription();
+        }
+    }
+}
